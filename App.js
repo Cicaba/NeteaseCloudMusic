434dd305c842220370477af18559f66439c36c60
Cicaba <1533436877@qq.com>
2018-11-09 17:50:08 +0800

    header

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text,Image, View} from 'react-native';
+import {Platform, StyleSheet, Text,StatusBar, View} from 'react-native';
 import { Button ,Header } from 'react-native-elements'
 //测试平台
 const instructions = Platform.select({
@@ -18,35 +18,37 @@ const instructions = Platform.select({
 export default class App extends Component {
   constructor(Props){
     super(Props)
-    this.state={text:"明天你好!我在开发!"}
+    this.state={
+      text:"明天你好!我在开发!",
+      headerState : 2
+    }
   }
   onLongPress(e){
     this.setState({text:"明天你好!我在开发yayaya!"})
   }
+  changeMenu(n){
+    this.setState({headerState:n})
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Header
-          backgroundColor="#f03"
-          outerContainerStyles={{height:54}}
-          innerContainerStyles={{alignItems:"center"}}
-          leftComponent={{ icon: 'menu', color: '#fff',width:32 }}
-          centerComponent={{ text: 'MY TITLE', style: { color: '#fff',width:300,textAlignVertical:"center", textAlign:"center",includeFontPadding:false } }}
-          // centerComponent={<View><Image source={require('./android/app/src/main/res/drawable/music-note.png')}/></View>}
-          rightComponent={{ icon: 'search', color: '#fff',width:32 }}
-        />
-        {/*<Text style={styles.welcome} selectable={true}>{this.state.text}</Text>*/}
-        {/*<Text style={styles.instructions}>{instructions}</Text>*/}
-        {/*<Button*/}
-          {/*icon={{name: 'envira', type: 'font-awesome'}}*/}
-          {/*rounded={true}*/}
-          {/*backgroundColor={'#0f0'}*/}
-          {/*underlayColor={'#f60'}*/}
-          {/*disabled={false}*/}
-          {/*onLongPress={(e)=>{this.onLongPress(e)}}*/}
-          {/*borderRadius={1}*/}
-          {/*textStyle={{color:"#f60"}}*/}
-          {/*title='登陆' />*/}
+        {/*导航栏颜色*/}
+        <StatusBar backgroundColor={'#ff1a21'}></StatusBar>
+        {/*头部导航*/}
+        <View style={styles.header}>
+          <View style={{width:"10%"}}>
+            <Text onPress={()=>{this.changeMenu(1)}} style={this.state.headerState==1 ? styles.selectIcon : styles.headerIcon}>&#xf008;</Text>
+          </View>
+          <View style={styles.headerCenter}>
+            <Text onPress={()=>{this.changeMenu(2)}} style={this.state.headerState==2 ? styles.selectIcon : styles.headerIcon}>&#xe802;</Text>
+            <Text onPress={()=>{this.changeMenu(3)}} style={this.state.headerState==3 ? styles.selectIcon : styles.headerIcon}>&#xe803;</Text>
+            <Text onPress={()=>{this.changeMenu(4)}} style={this.state.headerState==4 ? styles.selectIcon : styles.headerIcon}>&#xe801;</Text>
+          </View>
+          <View style={{width:"10%"}}>
+            <Text onPress={()=>{this.changeMenu(5)}} style={this.state.headerState==5 ? styles.selectIcon : styles.headerIcon}>&#xe800;</Text>
+          </View>
+        </View>
+        <Text>{this.state.headerState}</Text>
       </View>
     );
   }
@@ -59,15 +61,33 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'flex-start',
     backgroundColor: '#F5FCFF',
+    flexWrap:"wrap",
+    width:"100%"
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  header:{
+    alignItems:"center",
+    flexDirection:"row",
+    paddingLeft:20,
+    paddingRight:20,
+    height:50,
+    width:"100%",
+    backgroundColor:"#ff1a21"
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  headerCenter:{
+    width:"80%",
+    paddingLeft:"22%",
+    paddingRight:"22%",
+    flexDirection:"row",
+    justifyContent:"space-between"
   },
+  headerIcon:{
+    fontFamily:'fontello',
+    color:"#ccc",
+    fontSize:22
+  },
+  selectIcon:{
+    fontFamily:'fontello',
+    color:"#fff",
+    fontSize:22
+  }
 });
